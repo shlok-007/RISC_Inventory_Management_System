81a682c7e44324c7d6ebca87d069f7433282add6
Shlok Kumar Shaw <shlok845@gmail.com>
2024-04-17 03:08:52 +0530

    asf

--- risc_inventory/src/components/ui/card-hover-effect.tsx
+++ risc_inventory/src/components/ui/card-hover-effect.tsx
@@ -56,10 +56,7 @@ export const HoverEffect = ({
               />
             )}
           </AnimatePresence>
-          <Card>
-            <CardTitle>{item.title}</CardTitle>
-            <CardDescription>{item.description}</CardDescription>
-          </Card>
+          <Card {...item} />
         </div>
       ))}
     </div>
@@ -96,53 +93,50 @@ export const HoverEffect = ({
 */
 
 export const Card = ({
-  className,
-  children,
+  image,
+  title,
+  description,
+  version,
+  category,
+  quantity
 }: {
-  className?: string;
-  children: React.ReactNode;
+  image: string;
+  title: string;
+  description: string;
+  version: string;
+  category: string;
+  quantity: number;
 }) => {
   return (
-    <div
-      className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
-      )}
-    >
-      <div className="relative z-50">
-        <div className="p-4">{children}</div>
-      </div>
+    <div>
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={image}
+          alt="product image"
+          height="400"
+          width="400"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          {title}
+        </p>
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {category}
+        </p>
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {"Version: "+version}
+        </p>
+        
+ 
+        {/* <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {description}
+        </p> */}
+        <button className="rounded-full px-4 py-2 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+          <span>Reserve now </span>
+        </button>
+      </BackgroundGradient>
     </div>
   );
 };
-export const CardTitle = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
-      {children}
-    </h4>
-  );
-};
-export const CardDescription = ({
-  className,
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <p
-      className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
-        className
-      )}
-    >
-      {children}
-    </p>
-  );
-};
+
+
