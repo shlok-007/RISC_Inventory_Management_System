51726b946421666fc9c6101784acaa7d56d70ecb
Sagnik-2003 <21cs02004@iitbbs.ac.in>
2024-04-17 19:51:01 +0530

    typewritter

--- risc_inventory/src/app/admin/page.tsx
+++ risc_inventory/src/app/admin/page.tsx
@@ -6,55 +6,54 @@ import { Tabs } from "@/components/ui/tabs";
 export default function TabsDemo() {
   const tabs = [
     {
-      title: "Product",
-      value: "product",
+      title: "Items",
+      value: "Items",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Product Tab</p>
+          <p>Items Tab</p>
+        </div>
+      ),
+    },
+    {
+      title: "Members",
+      value: "Members",
+      content: (
+        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+          <p>Members tab</p>
           <DummyContent />
         </div>
       ),
     },
     {
-      title: "Services",
-      value: "services",
+      title: "Governors",
+      value: "Governors",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Services tab</p>
+          <p>Governors tab</p>
           <DummyContent />
         </div>
       ),
     },
-    {
-      title: "Playground",
-      value: "playground",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Playground tab</p>
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Content",
-      value: "content",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Content tab</p>
-          <DummyContent />
-        </div>
-      ),
-    },
-    {
-      title: "Random",
-      value: "random",
-      content: (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Random tab</p>
-          <DummyContent />
-        </div>
-      ),
-    },
+    // {
+    //   title: "Content",
+    //   value: "content",
+    //   content: (
+    //     <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+    //       <p>Content tab</p>
+    //       <DummyContent />
+    //     </div>
+    //   ),
+    // },
+    // {
+    //   title: "Random",
+    //   value: "random",
+    //   content: (
+    //     <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
+    //       <p>Random tab</p>
+    //       <DummyContent />
+    //     </div>
+    //   ),
+    // },
   ];
 
   return (
