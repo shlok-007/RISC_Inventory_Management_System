26322c2e0ea02dcd08c5f2170ba780f94cd9d380
Shlok Kumar Shaw <shlok845@gmail.com>
2024-04-18 14:48:10 +0530

    Refactor login and signup pages to use JSX expressions for welcome messages

--- risc_inventory/src/app/signup/page.tsx
+++ risc_inventory/src/app/signup/page.tsx
@@ -45,10 +45,10 @@ export default function SignupForm() {
   return (
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black mt-36">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to RISC's Inventory
+        {"Welcome to RISC's Inventory"}
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Sign up to RISC's Inventory Management System 
+        {"Sign up to RISC's Inventory Management System"} 
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
