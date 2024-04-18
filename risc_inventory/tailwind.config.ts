b4a220518a821cacaa4ffd8cdc0e59b910b47081
Sagnik-2003 <21cs02004@iitbbs.ac.in>
2024-04-18 11:36:20 +0530

    Profile page

--- risc_inventory/tailwind.config.ts
+++ risc_inventory/tailwind.config.ts
@@ -4,6 +4,34 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 const svgToDataUri = require("mini-svg-data-uri");
 
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   darkMode: "class",
+//   theme: {
+//     extend: {
+//       animation: {
+//         shimmer: "shimmer 2s linear infinite",
+//       },
+//       keyframes: {
+//         shimmer: {
+//           from: {
+//             backgroundPosition: "0 0",
+//           },
+//           to: {
+//             backgroundPosition: "-200% 0",
+//           },
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 const config: Config = {
   // darkMode: ["class"],
   content: [
@@ -74,12 +102,24 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        shimmer: 'shimmer 2s linear infinite',
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [
     require("tailwindcss-animate"),
