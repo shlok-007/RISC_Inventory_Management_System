f6ba20a6e6019b35a94f1c1992e03cff423a00cd
Shlok Kumar Shaw <shlok845@gmail.com>
2024-04-18 16:22:35 +0530

    Refactor UserContextWrapper to handle localStorage data retrieval

--- risc_inventory/src/context/index.tsx
+++ risc_inventory/src/context/index.tsx
@@ -1,29 +1,42 @@
-'use client'
-import { createContext, useState, useContext } from 'react';
+'use client';
+
+import { createContext, useState, useContext, useEffect } from 'react';
 
 interface UserData {
-    memberId: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    role: string;
+  memberId: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
 }
 
-const UserContext = createContext<{user: UserData | undefined, setUser: (user: UserData | undefined) => void}>({
-    user: undefined,
-    setUser: () => {}
+const UserContext = createContext<{
+  user: UserData | undefined;
+  setUser: (user: UserData | undefined) => void;
+}>({
+  user: undefined,
+  setUser: () => {},
 });
 
-export function UserContextWrapper({ children } : { children: React.ReactNode }) {
-    const [user, setUser] = useState<UserData | undefined>(localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData') as string) : undefined);
-    
-    return (
-        <UserContext.Provider value={{user, setUser}}>
-            {children}
-        </UserContext.Provider>
-    );
+export function UserContextWrapper({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<UserData | undefined>(undefined);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const userData = localStorage.getItem('userData');
+      if (userData) {
+        setUser(JSON.parse(userData));
+      }
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
 
 export function useUserContext() {
-    return useContext(UserContext);
+  return useContext(UserContext);
 }
